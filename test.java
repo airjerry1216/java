d21267f7e1530bb9cdede8261a8e2a81a24981f3
airjerry1216 <44132765+airjerry1216@users.noreply.github.com>
2019-06-27 00:00:24 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class test
+{
+    public static void main(String[] args)
+    {
+        gui g=new gui();
+        g.setSize(725,430);
+        g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g.setVisible(true);
+    }
+}
